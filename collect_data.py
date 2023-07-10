0461a0c5c65c0ad7beb09d396815f20ae61b10ca
an liu <xxmlala@qq.com>
2023-07-10 22:36:13 +0800

    add degree_yaw and degree_pitch as accordance of the done status of comeHereEnv; add the look_pitch adjustment for come2sth. data collection

--- collect_data.py
+++ collect_data.py
@@ -1,10 +1,6 @@
-# import hydra
-# import other_utils
+
 from taskEnv import ComeHereEnv
-# from agent.action_decoder import ActionDecoder
-# from agent.features import FeatureFusion
-# from agent.ppo import PPOAgent
-# import torch
+from ligent.server.server import set_object_counts
 import ligent
 import time
 import numpy as np
@@ -13,6 +9,8 @@ import matplotlib.pyplot as plt
 import os
 import math
 import h5py
+import argparse
+
 matplotlib.use('Agg')
 
 def save_pic(np_arr, pic_path):
@@ -23,34 +21,44 @@ def save_pic(np_arr, pic_path):
     plt.savefig(pic_path,bbox_inches='tight', pad_inches=0, transparent=True) 
     plt.close()
 
-def distance(a,b):
-    return ((a[0]-b[0])**2 + (a[1]-b[1])**2)**0.5
 
-def inference_action(info, collect_object:str='player'):
+def inference_come_to_sth_action(info, goal_object:str='player'):
     position_mate = info['game_states']['playmate']['position']
+    position_camera_mate = info['game_states']['playmateCamera']['position']
+    face_direction_camera_mate = info['game_states']['playmateCamera']['forward']
     face_direction_mate = info['game_states']['playmate']['forward']
 
-    if collect_object=='player':
-        position_player = info['game_states']['player']['position']
+    if goal_object=='player':
+        position_goal = info['game_states']['player']['position']
         face_direction_player = info['game_states']['player']['forward']
+        position_camera_player = info['game_states']['playerCamera']['position']
+
+        x2z_norm = ((position_camera_player['x']-position_camera_mate['x'])**2 + \
+                    (position_camera_player['z']-position_camera_mate['z'])**2)**0.5
+        camera_mate2goal = np.array([position_camera_player['y']-position_camera_mate['y'], x2z_norm])
     else:
-        instances_list = [i['prefab'].split('_')[1] for i in info['game_states']['instances']]
-        collect_object_ids = []
+        instances_list = [i['prefab'] for i in info['game_states']['instances']]
+        goal_object_ids = []
         for i,instance in enumerate(instances_list):
-            if instance==collect_object:
-                collect_object_ids.append(i)
-        assert len(collect_object_ids)==1,f"len(collect_object_idx) is {len(collect_object_ids)}"
-        collect_object_id = collect_object_ids[0]
-        position_player = info['game_states']['instances'][collect_object_id]['position']
-        face_direction_player = info['game_states']['instances'][collect_object_id]['forward']
+            # if instance==goal_object: #Pumpkin(Clone) == Pumpkin
+            if goal_object in instance:
+                goal_object_ids.append(i)
+        assert len(goal_object_ids)==1,f"len(collect_object_idx) is {len(goal_object_ids)}"
+        goal_object_id = goal_object_ids[0]
+        position_goal = info['game_states']['instances'][goal_object_id]['position']
+        face_direction_player = info['game_states']['instances'][goal_object_id]['forward']
 
-    mate2player = np.array([position_player['x']-position_mate['x'], position_player['z']-position_mate['z']])
-    mate2player /= (mate2player[0]**2 + mate2player[1]**2)**0.5
+        x2z_norm = ((position_goal['x']-position_camera_mate['x'])**2 + \
+                    (position_goal['z']-position_camera_mate['z'])**2)**0.5
+        camera_mate2goal = np.array([position_goal['y']-position_camera_mate['y'], x2z_norm])
+    
+    def vector_norm(v):
+        assert len(v)==2, len(v)
+        return (v[0]**2+v[1]**2)**0.5
 
-    mate_direction = np.array([face_direction_mate['x'], face_direction_mate['z']])
-    mate_direction /= (mate_direction[0]**2 + mate_direction[1]**2)**0.5
-
-    def deg(v1,v2):
+    def deg(v1, v2):
+        v1 = v1/vector_norm(v1)
+        v2 = v2/vector_norm(v2)
         # 计算向量的点积 
         dot_product = v1[0] * v2[0] + v1[1] * v2[1] # 计算向量的叉积 
         cross_product = v1[0] * v2[1] - v1[1] * v2[0] # 计算带符号夹角（弧度） 
@@ -58,78 +66,125 @@ def inference_action(info, collect_object:str='player'):
         ret_degree = angle/math.pi * 180
         # print(degree)
         return ret_degree
-    
-    degree = deg(mate2player, mate_direction)
-    # inner_product = lambda a,b: max(min(a[0]*b[0]+a[1]*b[1], 1), -1)
-    # outer_product = lambda a,b: a[0]*b[1]-a[1]*b[0]
-    # degree = math.acos(inner_product(mate_direction, mate2player)) / math.pi * 180
+
+
+    mate2goal = np.array([position_goal['x']-position_mate['x'], position_goal['z']-position_mate['z']])
+    face_direction_mate = np.array([face_direction_mate['x'], face_direction_mate['z']])
+    x2z_norm = (face_direction_camera_mate['x']**2 + face_direction_camera_mate['z']**2)**0.5
+    face_direction_camera_mate = np.array([face_direction_camera_mate['y'],x2z_norm])
+
+    degree_yaw = deg(mate2goal, face_direction_mate)
+    degree_pitch = deg(camera_mate2goal, face_direction_camera_mate)
 
     action_encoder = {
-        # 'no_op': 0,
-        'move_forward': 0,
-        'look_yaw_n30d': 1,
-        'look_yaw_p30d': 2,
-        'look_pitch_n30d': 3,
-        'look_pitch_p30d': 4,
-        'is_grab': 5,
-        'is_speak': 6
+        'no_op': 0,
+        'move_forward': 1,
+        'look_yaw_n30d': 2,
+        'look_yaw_n8d': 3,
+        'look_yaw_p8d': 4,
+        'look_yaw_p30d': 5,
+        'look_pitch_n10d': 6,
+        'look_pitch_p10d': 7,
+        'is_grab': 8,
+        'is_speak': 9
     }
 
-    if abs(degree) <= 15:
-        action_env = {
-            "move_right": 0,
-            "move_forward": 1,
-            "look_yaw": 0.0,
-            "look_pitch": 0.0,
-            "jump": False,
-            "grab": False,
-            "speak": "",
-            }
-        action_str = 'move_forward'
-    elif degree>=-30 and degree<-15:
-        action_env = {
-            "move_right": 0,
-            "move_forward": 0,
-            "look_yaw": -30,
-            "look_pitch": 0.0,
-            "jump": False,
-            "grab": False,
-            "speak": "",
-            }
-        action_str = "look_yaw_n30d"
+    def get_look_yaw(cur_degree_yaw, tolerance, eps=1):
+        assert abs(cur_degree_yaw) > tolerance+eps, f"cur_degree_yaw={cur_degree_yaw}, tolerance={tolerance}"
+        if cur_degree_yaw>=-(tolerance*2) and degree_yaw<-tolerance-eps:
+            action_env = {
+                "move_right": 0,
+                "move_forward": 0,
+                "look_yaw": -tolerance*2,
+                "look_pitch": 0.0,
+                "jump": False,
+                "grab": False,
+                "speak": "",
+                }
+            action_str = f"look_yaw_n{int(tolerance*2)}d"
+        else: # cur_degree_yaw >= tolerance+eps or cur_degree_yaw<-tolerance*2:
+            action_env = {
+                "move_right": 0,
+                "move_forward": 0,
+                "look_yaw": tolerance*2,
+                "look_pitch": 0.0,
+                "jump": False,
+                "grab": False,
+                "speak": "",
+                }
+            action_str = f'look_yaw_p{int(tolerance*2)}d'
+        return action_env, action_str
+    eps = 1
+    if abs(degree_yaw) <= 15+eps:
+        if abs(degree_yaw) > 4+eps:
+            action_env, action_str = get_look_yaw(degree_yaw, 4)
+        elif abs(degree_pitch) <= 5+eps:
+            action_env = {
+                "move_right": 0,
+                "move_forward": 1,
+                "look_yaw": 0.0,
+                "look_pitch": 0.0,
+                "jump": False,
+                "grab": False,
+                "speak": "",
+                }
+            action_str = 'move_forward'
+        elif degree_pitch <-5-eps:
+            action_env = {
+                "move_right": 0,
+                "move_forward": 0,
+                "look_yaw": 0.0,
+                "look_pitch": 10,
+                "jump": False,
+                "grab": False,
+                "speak": "",
+                }
+            action_str = 'look_pitch_p10d'
+        else:
+            action_env = {
+                "move_right": 0,
+                "move_forward": 0,
+                "look_yaw": 0.0,
+                "look_pitch": -10,
+                "jump": False,
+                "grab": False,
+                "speak": "",
+                }
+            action_str = 'look_pitch_n10d'
     else:
-        action_env = {
-            "move_right": 0,
-            "move_forward": 0,
-            "look_yaw": 30,
-            "look_pitch": 0.0,
-            "jump": False,
-            "grab": False,
-            "speak": "",
-            }
-        action_str = 'look_yaw_p30d'
+        action_env, action_str = get_look_yaw(degree_yaw, 15)
     
     return action_env, action_encoder[action_str], action_str
 
 def collect(env,episodes:int, collect_object:str='player'):
-    if not (collect_object == 'player'):
-        instruction = f"come to the {collect_object}"
+    if collect_object != 'player':
+        set_object_counts({collect_object: 1})
+        instruction = f"come to the {collect_object.split('_')[0]}"
     else:
         instruction = "come here"
     
-    print(f"instruction: {instruction}")
-    env_decoder = ComeHereEnv(goal_object=collect_object,distance_reward=10, success_reward=200, distance_min=1.2, step_penalty=1, episode_len=100, is_debug=True)
+    print(f"instruction: come to the {collect_object}, labels_instruction: {instruction}", flush=True)
+    env_decoder = ComeHereEnv(goal_object=collect_object,distance_reward=10, success_reward=200, distance_min=1.2,
+                               step_penalty=1, episode_len=100, look_yaw_tolerance=15,look_pitch_tolerance=5,eps=0.5, is_debug=True)
 
     action_noop = {
-            "move_right": 0,
-            "move_forward": 0,
-            "look_yaw": 0.0,
-            "look_pitch": 0.0,
-            "jump": False,
-            "grab": False,
-            "speak": "",
-            }
-    returns = []
+        "move_right": 0,
+        "move_forward": 0,
+        "look_yaw": 0.0,
+        "look_pitch": 0.0,
+        "jump": False,
+        "grab": False,
+        "speak": "",
+        }
+    action_grab = {
+        "move_right": 0,
+        "move_forward": 0,
+        "look_yaw": 0.0,
+        "look_pitch": 0.0,
+        "jump": False,
+        "grab": True,
+        "speak": "",
+    }
 
     if not os.path.exists('./dataset'):
         os.makedirs('./dataset')
@@ -138,114 +193,74 @@ def collect(env,episodes:int, collect_object:str='player'):
         data_nums = 0
         fail_nums = 0
         success_episode, fail_episode = 0,0
-        obs_V_set = f.create_dataset('obs_V', (0,224,224,3), maxshape=(None, 224, 224, 3), dtype='i')
-        obs_T_set = f.create_dataset('obs_T', (0,), maxshape=(None,),dtype=h5py.string_dtype("utf-8")) #TODO 往出拿时需要 “ss”.decode("utf-8")
-        action_set = f.create_dataset('action', (0,), maxshape=(None,),dtype='i')
-        # data_obs_V = []
-        # data_obs_T = []
-        # data_action = []
+        obs_V_set, obs_T_set, action_set = [],[],[]
+        success_grab = 0
         for episode in range(episodes):
-            if episode%5==0:
-                print(f"Episode: {episode}/{episodes}")
-            path = f"./obs_visions_{collect_object}/episode_{episode}"
-            if not os.path.exists(path):
-                os.makedirs(path)
+            if episode%50==0:
+                print(f"Episode: {episode}/{episodes}", flush=True)
+            
             state_img, _ = env.reset()
             env_decoder.reset()
             done, blocked = False, False
             (state_img, state_text), _, _, info = env.step(**action_noop)
-            
             obs_list = []
 
             while not (done or blocked):
                 last_state_img = state_img
                 last_state_text = state_text
                 last_state_text = instruction
-                action_env, action_code, action_str = inference_action(info, collect_object=collect_object)
+                action_env, action_code, action_str = inference_come_to_sth_action(info, goal_object=collect_object)
                 (state_img, state_text), _, _, info = env.step(**action_env)
                 reward, done, blocked, cumulate_reward, elspsed_step, distance_info = env_decoder.step(info)
-                obs_list.append((last_state_img, last_state_text, action_code, path+f'/{elspsed_step:03d}_{action_str}_{round(reward,1)}.png'))
-
-            # obs_list.append((state_img, instruction, 0, path+f'/{elspsed_step+1:03d}_noOp_NULL.png'))
+                obs_list.append((last_state_img, last_state_text, action_code, f'/{elspsed_step:03d}_{action_str}_{round(reward,1)}.png'))
+            obs_list.append((state_img, instruction, 0, f'/{elspsed_step+1:03d}_noOp_NULL.png'))
             if blocked:
                 fail_nums += len(obs_list)
                 fail_episode += 1
-                print(f"BLOCKED_sum: {fail_nums}")
+                # print(f"BLOCKED_sum: {fail_nums}")
                 continue
             success_episode += 1
+            if instruction != "come here":
+                (_,_), _,_, info = env.step(**action_grab)
+                if not blocked and collect_object in info['game_states']['instances'][info['game_states']['playmateGrabInstance']]['prefab']:
+                    success_grab += 1
+                else:
+                    pass
+                    # not_grab_nums += len(obs_list)
+                    # continue
+            path = f"./obs_visions_{collect_object}/episode_{episode}_{elspsed_step:.0f}_{cumulate_reward:.0f}"
+            if not os.path.exists(path):
+                os.makedirs(path)
+
             for state_img,state_text,action_code,pic_path in obs_list:
                 data_nums += 1
-                obs_V_set.resize((data_nums, 224,224,3))
-                obs_T_set.resize((data_nums,))
-                action_set.resize((data_nums,))
-                obs_V_set[data_nums-1] = state_img
-                obs_T_set[data_nums-1] = instruction#state_text
-                action_set[data_nums-1] = action_code
-                save_pic(state_img, pic_path=pic_path)
+                obs_V_set.append(state_img)
+                obs_T_set.append(state_text)
+                action_set.append(action_code)
+                save_pic(state_img, pic_path=path+pic_path)
 
-            os.rename(path, path + f"_{elspsed_step}_{distance_info}_{cumulate_reward}")
-    print(returns)
+            # os.rename(path, path + f"_{elspsed_step}_{distance_info}_{cumulate_reward}")
+        f.create_dataset('obs_V', data=np.array(obs_V_set).astype(np.uint8), compression='gzip', compression_opts=9)
+        # f.create_dataset('obs_T', (0,), maxshape=(None,),dtype=h5py.string_dtype("utf-8")) #TODO 往出拿时需要 “ss”.decode("utf-8")
+        f.create_dataset('obs_T', data=obs_T_set)
+        f.create_dataset('action', data=action_set)
     env.close()
-    print(f"{success_episode} success_episodes, {fail_episode} fail_episodes, {data_nums} data pairs.")
-    
-def debug(env):
-    action_noop = {
-            "move_right": 0,
-            "move_forward": 0,
-            "look_yaw": 0.0,
-            "look_pitch": 0.0,
-            "jump": False,
-            "grab": False,
-            "speak": "",
-            }
-    action_right15 = {
-            "move_right": 0,
-            "move_forward": 1,
-            "look_yaw": 0,
-            "look_pitch": 0.0,
-            "jump": False,
-            "grab": False,
-            "speak": "",
-            }
-    ligent.set_scenes_dir("C:/Users/19355/Desktop/drlProject/LIGENT/custom_scenes")
-    # env = ligent.Environment(path="C:/Users/19355/Desktop/drlProject/05272014_fix_multi_rotate/305272014_fix_multi_rotate/LIGENT.exe")
-    env_decoder = ComeHereEnv(distance_reward=10, success_reward=200, distance_min=1.2, step_penalty=1, episode_len=100, is_debug=True)
-    (state_img, _), _, _, info = env.step(**action_noop)
-    print(info['game_states']['playmate']['forward'], flush=True)
-    last_forward = info['game_states']['playmate']['forward']
-    last_position = info['game_states']['playmate']['position']
-    last_time = time.time()
-    for step in range(360//15):
-        (state_img, _), _, _, info = env.step(**action_right15)
-        print(f"Cost {time.time()-last_time} s!")
-        last_time = time.time()
-        # reward, done, blocked, cumulate_reward, elspsed_step, distance_info = env_decoder.step(info)
-        print(info['game_states']['playmate']['forward'], flush=True)
-        current_forward = info['game_states']['playmate']['forward']
-        current_position = info['game_states']['playmate']['position']
-        print(degree([last_forward['x'],last_forward['z']],[current_forward['x'],current_forward['z']]))
-        print(distance([current_position['x'],current_position['z']],[last_position['x'],last_position['z']]))
-        # time.sleep(3)
-        last_forward = current_forward
-        last_position = current_position
-
-    env.close()
-    ligent.set_scenes_dir("")
+    print(f"Blocked_num: {fail_nums}",flush=True)
+    print(f"{success_grab}/{success_episode} success_grab/success_episodes = {success_grab/success_episode}, {fail_episode} fail_episodes, {data_nums} data pairs.", flush=True)
 
 if __name__ == "__main__":
-    
-
-    ligent.set_scenes_dir("")
-    env = ligent.Environment(path="C:/Users/19355/Desktop/drlProject/ligent-windows/06061943_win_224/LIGENT.exe")
-    collect(env,1000, collect_object='ChristmasTree')
-    exit(0)
-    # with Display(visible=False) as disp:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--goal_object", type=str, default="ChristmasTree_01")
+    parser.add_argument("--episodes_num", type=int, default=50)
+    args = parser.parse_args()
+    # ligent.set_scenes_dir("")
+    # env = ligent.Environment(path="C:/Users/19355/Desktop/drlProject/ligent-windows/06061943_win_224/LIGENT.exe")
+    # collect(env,50, collect_object='Pumpkin')
+    # collect(env,args.episodes_num, collect_object=args.goal_object)
     try:
         ligent.set_scenes_dir("")
         env = ligent.Environment(path="C:/Users/19355/Desktop/drlProject/ligent-windows/06061943_win_224/LIGENT.exe")
-        collect(env,10)
-        # debug(env)
-    except:
-        print("Some error happened!")
+        collect(env,args.episodes_num, collect_object=args.goal_object)
+    except Exception as e:
+        print(e, flush=True)
         env.close()
-    # debug()
--- taskEnv/come_here.py
+++ taskEnv/come_here.py
@@ -1,3 +1,5 @@
+import numpy as np
+import math
 class ComeHereEnv:
     def __init__(
         self,
@@ -6,6 +8,9 @@ class ComeHereEnv:
         distance_min: float | int,
         step_penalty: float | int,
         episode_len,
+        look_yaw_tolerance,
+        look_pitch_tolerance,
+        eps,
         goal_object: str='',
         is_debug=False
     ):
@@ -24,6 +29,10 @@ class ComeHereEnv:
         self._stay_static_steps = 0
         self._last_distance = 9999
 
+        self._look_yaw_tolerance = look_yaw_tolerance
+        self._look_pitch_tolerance = look_pitch_tolerance
+        self._eps = eps
+
         self._is_debug = is_debug
 
     def reset(self):
@@ -37,32 +46,62 @@ class ComeHereEnv:
 
     def step(self, info):
         self._elapsed_step += 1
-
         position_mate = info['game_states']['playmate']['position']
+        position_camera_mate = info['game_states']['playmateCamera']['position']
+        face_direction_camera_mate = info['game_states']['playmateCamera']['forward']
+        face_direction_mate = info['game_states']['playmate']['forward']
 
         if self._goal_object=='player':
-            position_player = info['game_states']['player']['position']
+            position_goal = info['game_states']['player']['position']
+            position_camera_player = info['game_states']['playerCamera']['position']
+            x2z_norm = ((position_camera_player['x']-position_camera_mate['x'])**2 + \
+                        (position_camera_player['z']-position_camera_mate['z'])**2)**0.5
+            camera_mate2goal = np.array([position_camera_player['y']-position_camera_mate['y'], x2z_norm])
         else:
-            instances_list = [i['prefab'].split('_')[1] for i in info['game_states']['instances']]
+            instances_list = [i['prefab'] for i in info['game_states']['instances']]
             goal_object_ids = []
             for i,instance in enumerate(instances_list):
-                if instance==self._goal_object:
+                # if instance==self._goal_object:
+                if self._goal_object in instance:
                     goal_object_ids.append(i)
             assert len(goal_object_ids)==1,f"len(collect_object_idx) is {len(goal_object_ids)}, goal={self._goal_object}"
             goal_object_id = goal_object_ids[0]
-            position_player = info['game_states']['instances'][goal_object_id]['position']
+            position_goal = info['game_states']['instances'][goal_object_id]['position']
+            x2z_norm = ((position_goal['x']-position_camera_mate['x'])**2 + \
+                    (position_goal['z']-position_camera_mate['z'])**2)**0.5
+            camera_mate2goal = np.array([position_goal['y']-position_camera_mate['y'], x2z_norm])
+    
         # face_direction_player = info['game_states']['player']['forward']
         # face_direction_mate = info['game_states']['playmate']['forward']
-
+        def vector_norm(v):
+            assert len(v)==2, len(v)
+            return (v[0]**2+v[1]**2)**0.5
+        
+        def deg(v1, v2):
+            v1 = v1/vector_norm(v1)
+            v2 = v2/vector_norm(v2)
+            # 计算向量的点积 
+            dot_product = v1[0] * v2[0] + v1[1] * v2[1] # 计算向量的叉积 
+            cross_product = v1[0] * v2[1] - v1[1] * v2[0] # 计算带符号夹角（弧度） 
+            angle = math.atan2(cross_product, dot_product)
+            ret_degree = angle/math.pi * 180
+            # print(degree)
+            return ret_degree
+        
         def get_distance(player:dict, mate:dict):
             distance = ((player['x']-mate['x'])**2 + (player['z']-mate['z'])**2) ** 0.5
             return distance
-        
-        distance_current = get_distance(position_player, position_mate)
+        mate2goal = np.array([position_goal['x']-position_mate['x'], position_goal['z']-position_mate['z']])
+        face_direction_mate = np.array([face_direction_mate['x'], face_direction_mate['z']])
+        x2z_norm = (face_direction_camera_mate['x']**2 + face_direction_camera_mate['z']**2)**0.5
+        face_direction_camera_mate = np.array([face_direction_camera_mate['y'],x2z_norm])   
+        degree_yaw = deg(mate2goal, face_direction_mate)
+        degree_pitch = deg(camera_mate2goal, face_direction_camera_mate)
+
+        distance_current = get_distance(position_goal, position_mate)
         if self._gotten_min_distance == -1:
             self._init_distance = distance_current
             self._gotten_min_distance = distance_current
-
 
         if abs(distance_current-self._last_distance) < 0.1:
             self._stay_static_steps += 1
@@ -74,7 +113,11 @@ class ComeHereEnv:
         d_min_t = min(self._gotten_min_distance, distance_current)
 
         reward = self._distance_reward * max(self._gotten_min_distance-d_min_t, 0) - self._step_penalty
-        done = distance_current <= self._distance_min or self._elapsed_step >= self._episode_len
+        done = distance_current <= self._distance_min and \
+                abs(degree_pitch)<=self._look_pitch_tolerance+self._eps and \
+                abs(degree_yaw)<=self._look_yaw_tolerance+self._eps
+        
+        blocked = blocked or self._elapsed_step >= self._episode_len
         if distance_current <= self._distance_min:
             reward += self._success_reward
         self._cumulate_reward += reward
